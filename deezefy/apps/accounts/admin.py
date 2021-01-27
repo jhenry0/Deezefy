e4b18f886adb516acf67f09615dbb7bb48deba23
jhenry0 <jh.henri7@gmail.com>
2021-01-26 21:16:58 -0300

    pre deploy

--- deezefy/apps/accounts/admin.py
+++ deezefy/apps/accounts/admin.py
@@ -2,14 +2,6 @@ from django.contrib import admin
 from .models import User, Artist, Listener
 
 # Register your models here.
-class ArtistInline(admin.StackedInline):
-    model = Artist
-    fk_name = "user"
-    extra= 0
-
-class Listener(admin.StackedInline):
-    model = Listener
-
 
     
 admin.site.register(User)
